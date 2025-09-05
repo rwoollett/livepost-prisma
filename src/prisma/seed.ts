9dbbcde3c7da47610e960a39d1588143bdeb2181
Rodney Woollett <rodney.woollett@gmail.com>
2025-09-05 18:15:43 +1200

    use quotes in values

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -16,11 +16,11 @@ import { PrismaClient } from "@prisma/client";
   `;
 
   await prismaTest.$executeRaw`
-  INSERT INTO "Users" ("authId", "name") VALUES ("temp@hello.co.nz", "Temp User at Hello co nz");
+  INSERT INTO "Users" ("authId", "name") VALUES ('temp@hello.co.nz', 'Temp User at Hello co nz');
   `;
 
   await prismaTest.$executeRaw`
-  INSERT INTO "Posts" ("title", "content", "userId", "date") VALUES ("Some tough spiders are thought of simply as figs", "Their bird was, in this moment, a silly bear. They were lost without the amicable kiwi that composed their fox. A peach is an amicable crocodile. A tidy fox without sharks is truly a scorpion of willing cats. Shouting with happiness, a currant is a wise currant!", 1, NOW());
+  INSERT INTO "Posts" ("title", "content", "userId", "date") VALUES ('Some tough spiders are thought of simply as figs', 'Their bird was, in this moment, a silly bear. They were lost without the amicable kiwi that composed their fox. A peach is an amicable crocodile. A tidy fox without sharks is truly a scorpion of willing cats. Shouting with happiness, a currant is a wise currant!', 1, NOW());
   `;
 
   prismaTest.$disconnect();
